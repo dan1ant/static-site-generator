edc195296f1f77af9b8b445de5c5d1f81e3e0a64
dan1ant <21098537+dan1ant@users.noreply.github.com>
2025-03-11 17:44:52 +0100

    CH5: add subpages and new file generator

--- src/main.py
+++ src/main.py
@@ -1,12 +1,14 @@
 import os
 import shutil
+from pathlib import Path
 
 from block import markdown_to_html_node
 from extractor import extract_title
 
 def main():
     copy_folder("static", "public")
-    generate_page("content/index.md", "template.html", "public/index.html")
+    # generate_page("content/index.md", "template.html", "public/index.html")
+    generate_pages_recursive("content", "template.html", "public")
 
 def copy_folder(src_dir, dst_dir):
     if os.path.exists(dst_dir):
@@ -27,8 +29,8 @@ def copy_folder(src_dir, dst_dir):
 
 def generate_page(from_path, template_path, dest_path):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
-    from_content = ""
-    template_content = ""
+    from_content = None
+    template_content = None
     with open(from_path) as f:
         from_content = f.read()
     with open(template_path) as f:
@@ -39,6 +41,31 @@ def generate_page(from_path, template_path, dest_path):
     template_content = template_content.replace("{{ Title }}", title).replace("{{ Content }}", html_content)
     with open(dest_path, "w") as f:
         f.write(template_content)
-   
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    items = os.listdir(dir_path_content)    
+    template_content = None
+    with open(template_path) as f:
+        template_content = f.read()   
+
+    for item in items:
+        src_path = Path(dir_path_content, item)
+        dst_path = Path(dest_dir_path, item.replace(".md", ".html"))
+
+        if os.path.isfile(src_path):
+            print(f"Generating page from {src_path} to {dst_path} using {template_path}")
+            src_content = None
+            with open(src_path) as f:
+                src_content = f.read()
+            title = extract_title(src_content)
+            html = markdown_to_html_node(src_content).to_html()
+            dst_content = template_content.replace("{{ Title }}", title).replace("{{ Content }}", html)
+            with open(dst_path, "w") as f:
+                f.write(dst_content)
+        else:
+            print(f"Creating directory: {dst_path}")
+            os.mkdir(dst_path)
+            generate_pages_recursive(src_path, template_path, dst_path)
+
 if __name__ == "__main__":
     main()
